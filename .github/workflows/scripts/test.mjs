e5ccf94aae459b90d504a7dffc43c4822220df7f
Chiamaka Nwolisa <cnwo@hellofresh.com>
2022-02-28 15:37:22 +0100

    add env to zx script

--- .github/workflows/scripts/test.mjs
+++ .github/workflows/scripts/test.mjs
@@ -1,3 +1,9 @@
 #!/usr/bin/env zx
 
+const { GITHUB_REPOSITORY, PR_LINK, GITHUB_ACTOR, PR_TITLE } = process.env;
+
 console.log('testing testing 123');
+console.log('GITHUB_REPOSITORY', GITHUB_REPOSITORY);
+console.log('PR_LINK', PR_LINK)
+console.log('GITHUB_ACTOR', GITHUB_ACTOR);
+console.log('PR_TITLE', PR_TITLE)
