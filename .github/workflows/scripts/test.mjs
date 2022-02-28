95c5e3680457059f14665623558d0f160f0edd92
Chiamaka Nwolisa <cnwo@hellofresh.com>
2022-02-28 17:51:43 +0100

    add slack api

--- .github/workflows/scripts/test.mjs
+++ .github/workflows/scripts/test.mjs
@@ -1,6 +1,32 @@
 #!/usr/bin/env zx
 
-const { GITHUB_REPOSITORY, PR_LINK, GITHUB_ACTOR, PR_TITLE } = process.env;
+import { WebClient } from '@slack/web-api';
+
+const { GITHUB_REPOSITORY, PR_LINK, GITHUB_ACTOR, PR_TITLE, SLACK_TOKEN } = process.env;
+const client = new WebClient(SLACK_TOKEN);
+
+await client.chat.postMessage({
+  channel: '#dependabot-alerts',
+  attachments: [
+    {
+      color: "#c90200",
+      title: `:robots: A Dependabot PR failed to be merged in ${GITHUB_REPOSITORY} :thumbsdown:`,
+      actions: [
+        {
+          "text": "Jira Ticket",
+          "type": "button",
+          "url": JIRA_ISSUE
+        },
+        {
+          "text": "Pull request",
+          "type": "button"
+          "url": PR_LINK
+        }
+      ]
+    }
+  ]
+});
+
 
 console.log('testing testing 123');
 console.log('GITHUB_REPOSITORY', GITHUB_REPOSITORY);
